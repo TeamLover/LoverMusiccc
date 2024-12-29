7029023b86c3e2296be360564cbe460b498f882c
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-12-29 22:50:21 +0530

    Update help.py

--- AviaxMusic/utils/inline/help.py
+++ AviaxMusic/utils/inline/help.py
@@ -14,77 +14,62 @@ def help_pannel(_, START: Union[bool, int] = None):
         ),
     ]
     mark = second if START else first
-    mark = second if START else first
     upl = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text=_["H_B_1"],
+                    text="💥ᴀᴅᴍɪɴ",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_2"],
+                    text="💥ᴀᴜᴛʜ💥",
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_3"],
+                    text="💥ʙʟᴀᴄᴋʟɪsᴛ💥",
                     callback_data="help_callback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_4"],
+                    text="💥ʙʀᴏᴀᴅᴄᴀsᴛ☀️",
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
+                    text="💥ɢʙᴀɴ💥",
+                    callback_data="help_callback hb12",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_6"],
+                    text="💥ʟʏʀɪᴄs💥",
+                    callback_data="help_callback hb5",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="💥ᴩɪɴɢ💥",
+                    callback_data="help_callback hb7",
+                ),
+                InlineKeyboardButton(
+                    text="💥ᴩʟᴀʏ💥",
+                    callback_data="help_callback hb8",
+                ),
+                InlineKeyboardButton(
+                    text="💥ᴩʟᴀʏʟɪsᴛ💥",
                     callback_data="help_callback hb6",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
+                    text="💥ᴠɪᴅᴇᴏᴄʜᴀᴛs💥",
                     callback_data="help_callback hb10",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_11"],
+                    text="💥sᴛᴀʀᴛ💥",
                     callback_data="help_callback hb11",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
+                    text="💥sᴜᴅᴏ💥",
+                    callback_data="help_callback hb9",
                 ),
             ],
             mark,
