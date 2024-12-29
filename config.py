2f9c150aeeee42a3dd9b7e99aa24ac304fdf96f6
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-12-30 01:55:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -105,11 +105,6 @@ def time_to_seconds(time):
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
-        )
 
 if SUPPORT_GROUP:
     if not re.match("(?:http|https)://", SUPPORT_GROUP):
