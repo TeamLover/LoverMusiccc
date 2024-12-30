238945635e37df7667a4da8c2aff5fa5719c7c06
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-12-30 10:32:17 +0530

    Update stream.py

--- AviaxMusic/utils/stream/stream.py
+++ AviaxMusic/utils/stream/stream.py
@@ -9,8 +9,7 @@ from AviaxMusic import Carbon, YouTube, app
 from AviaxMusic.core.call import Aviax
 from AviaxMusic.misc import db
 from AviaxMusic.utils.database import (music_on)
-from AviaxMusic.utils.exceptions import AssistantErr
-from AviaxMusic.utils.inline.playlist 
+from AviaxMusic.utils.exceptions import AssistantErr 
 from AviaxMusic.utils.pastebin import Anonbin
 from AviaxMusic.utils.stream.queue import put_queue, put_queue_index
 from AviaxMusic.utils.thumbnails import gen_thumb, gen_qthumb
