c3c80a36c177658b037d99a8eaaa516f3d0d9e9f
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-12-30 13:16:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_ID = int(getenv("API_ID", "22926746"))
 API_HASH = getenv("API_HASH", "ffd91926f59e55fb08b3e1a4f5b99b1d")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "5983601762:AAE_IgDImcDoRMVym7of7udcTnVgXsVK3t4")
+BOT_TOKEN = getenv("BOT_TOKEN", "5983601762:AAE-3OKJ0wpkKzbNA3rKphqntVBG-qAI4bw")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
