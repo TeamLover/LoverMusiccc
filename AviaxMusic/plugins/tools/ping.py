2926799dd0322360ed85afb48654d6a1c90d73e8
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-04 02:22:01 +0530

    Update ping.py

--- AviaxMusic/plugins/tools/ping.py
+++ AviaxMusic/plugins/tools/ping.py
@@ -3,26 +3,34 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
 from AviaxMusic import app
-from AviaxMusic.core.call import Aviax
+from AviaxMusic.core.call import Anon
 from AviaxMusic.utils import bot_sys_stats
 from AviaxMusic.utils.decorators.language import language
-from AviaxMusic.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from AviaxMusic.utils.inline.play import close_keyboard
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(
+    filters.command(PING_COMMAND)
+)
 @language
 async def ping_com(client, message: Message, _):
-    start = datetime.now()
     response = await message.reply_photo(
         photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
+        caption=_["ping_1"],
     )
-    pytgping = await Aviax.ping()
+    start = datetime.now()
+    pytgping = await Anon.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
+        _["ping_2"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ),
+        reply_markup=close_keyboard
     )
