2858921046a0216e95e1a56d22b9a646383eab4d
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-11 19:50:17 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -8,7 +8,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps, ImageChops
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+from config import YOUTUBE_IMG_URL
 from back import files
 
 def changeImageSize(maxWidth, maxHeight, image):
