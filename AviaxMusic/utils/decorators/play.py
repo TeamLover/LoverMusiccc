58413993c5998cde2d411a6fbad25e45fc206f50
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-13 20:30:27 +0530

    Update play.py

--- AviaxMusic/utils/decorators/play.py
+++ AviaxMusic/utils/decorators/play.py
@@ -21,7 +21,7 @@ from AviaxMusic.utils.database import (
     is_maintenance,
 )
 from AviaxMusic.utils.inline import botplaylist_markup
-from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
+from config import PLAYLIST_IMG_URL,  adminlist
 from strings import get_string
 
 links = {}
