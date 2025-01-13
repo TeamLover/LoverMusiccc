36dd85ce53b96f3c631b7f5d9bbc4ec07b11a0c0
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-13 16:38:37 +0530

    Update speedtest.py

--- AviaxMusic/plugins/tools/speedtest.py
+++ AviaxMusic/plugins/tools/speedtest.py
@@ -1,45 +1,50 @@
 import asyncio
-
 import speedtest
 from pyrogram import filters
-from pyrogram.types import Message
-
+from strings import get_command
 from AviaxMusic import app
 from AviaxMusic.misc import SUDOERS
-from AviaxMusic.utils.decorators.language import language
+
+# Commands
+SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
 
 
-def testspeed(m, _):
+def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit_text(_["server_12"])
+        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...**")
         test.download()
-        m = m.edit_text(_["server_13"])
+        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴜᴩʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...**")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit_text(_["server_14"])
+        m = m.edit("**↻ sʜᴀʀɪɴɢ sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs...**")
     except Exception as e:
-        return m.edit_text(f"<code>{e}</code>")
+        return m.edit(e)
     return result
 
 
-@app.on_message(filters.command(["speedtest", "spt"]) & SUDOERS)
-@language
-async def speedtest_function(client, message: Message, _):
-    m = await message.reply_text(_["server_11"])
+@app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
+async def speedtest_function(client, message):
+    m = await message.reply_text("💫 ᴛʀʏɪɴɢ ᴛᴏ ᴄʜᴇᴄᴋ ᴜᴩʟᴏᴀᴅ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅ...")
     loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m, _)
-    output = _["server_15"].format(
-        result["client"]["isp"],
-        result["client"]["country"],
-        result["server"]["name"],
-        result["server"]["country"],
-        result["server"]["cc"],
-        result["server"]["sponsor"],
-        result["server"]["latency"],
-        result["ping"],
+    result = await loop.run_in_executor(None, testspeed, m)
+    output = f"""✯ **sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs** ✯
+    
+<u>**❥͜͡ᴄʟɪᴇɴᴛ :**</u>
+**» __ɪsᴩ :__** {result['client']['isp']}
+**» __ᴄᴏᴜɴᴛʀʏ :__** {result['client']['country']}
+  
+<u>**❥͜͡sᴇʀᴠᴇʀ :**</u>
+**» __ɴᴀᴍᴇ :__** {result['server']['name']}
+**» __ᴄᴏᴜɴᴛʀʏ :__** {result['server']['country']}, {result['server']['cc']}
+**» __sᴩᴏɴsᴏʀ :__** {result['server']['sponsor']}
+**» __ʟᴀᴛᴇɴᴄʏ :__** {result['server']['latency']}  
+**» __ᴩɪɴɢ :__** {result['ping']}"""
+    msg = await app.send_photo(
+        chat_id=message.chat.id, 
+        photo=result["share"], 
+        caption=output
     )
-    msg = await message.reply_photo(photo=result["share"], caption=output)
     await m.delete()
